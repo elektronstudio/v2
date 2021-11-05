f15758da996f65dc815e114969fbefd9ba6b8a26
kristjanjansen <kristjan.jansen@gmail.com>
2021-11-05 18:30:16 +0200

    check

--- src/pages/Newplayer.vue
+++ src/pages/Newplayer.vue
@@ -3,11 +3,12 @@ import { Player, Hls, DefaultUi } from "@vime/vue-next";
 import "@vime/core/themes/default.css";
 const src =
   "https://le21.babahhcdn.com/bb1150-le/x_live_1_c1.smil/playlist.m3u8";
+//
 </script>
 
 <template>
   <Player>
-    <Hls>
+    <Hls autoPiP>
       <source :data-src="src" type="application/x-mpegURL" />
     </Hls>
     <DefaultUi></DefaultUi>
