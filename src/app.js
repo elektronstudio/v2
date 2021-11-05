770c8fa1b4ef444acf3d16aa5c031b70562cfd2c
kristjanjansen <kristjan.jansen@gmail.com>
2021-11-05 18:25:39 +0200

    player

--- src/app.js
+++ src/app.js
@@ -13,6 +13,10 @@ const routes = [
     component: () => import(`./pages/Index.vue`),
   },
   {
+    path: "/newplayer",
+    component: () => import(`./pages/Newplayer.vue`),
+  },
+  {
     path: "/synth",
     component: () => import(`./pages/Synth.vue`),
   },
