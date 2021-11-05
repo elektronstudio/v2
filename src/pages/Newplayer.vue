e98fc359cdd31d1fa3c1ef5aa6127d185e56efc8
Kristjan Jansen <kristjan.jansen@gmail.com>
2021-11-05 18:32:34 +0200

    feat: new player (#140)

---
+++ src/pages/Newplayer.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Player, Hls, DefaultUi } from "@vime/vue-next";
+import "@vime/core/themes/default.css";
+const src =
+  "https://le21.babahhcdn.com/bb1150-le/x_live_1_c1.smil/playlist.m3u8";
+//
+</script>
+
+<template>
+  <Player>
+    <Hls autoPiP>
+      <source :data-src="src" type="application/x-mpegURL" />
+    </Hls>
+    <DefaultUi></DefaultUi>
+  </Player>
+</template>
